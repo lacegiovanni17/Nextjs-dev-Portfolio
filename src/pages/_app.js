6022fb5d373d13b47f55cd55657c0c6867625ca7
Chidike Henry <dk1online25@gmail.com>
2023-05-03 23:45:40 +0100

    added new feat

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -8,8 +8,14 @@ const montserrat = Montserrat({
 
 export default function App({ Component, pageProps }) {
   return (
-    <main className={`${montserrat.variable} font-mont`}>
-      <Component {...pageProps} />
-    </main>
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className={`${montserrat.variable} font-mont`}>
+        <Component {...pageProps} />
+      </main>
+    </>
   );
 }
