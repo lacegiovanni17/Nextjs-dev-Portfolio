6a9a2ac9836c6a15a2e2dd826c02bda86fc8eafc
Chidike Henry <dk1online25@gmail.com>
2023-05-09 14:58:50 +0100

    added new feat skills

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -4,10 +4,13 @@ import { motion } from "framer-motion";
 const Skill = ({ name, x, y }) => {
   return (
     <motion.div
-      className="flex items-center justify-center rounded-full font-semibold bg-dark text-light p-8 shadow-dark cursor-pointer"
+      className="flex items-center justify-center rounded-full font-semibold bg-dark text-light py-3 px-6 shadow-dark cursor-pointer absolute"
       whileHover={{ scale: 1.05 }}
+      initial={{ x: 0, y: 0 }}
+      animate={{ x: x, y: y }}
+      transition={{ duration: 1.5 }}
     >
-      Web
+      {name}
     </motion.div>
   );
 };
@@ -16,13 +19,24 @@ const Skills = () => {
   return (
     <>
       <h2 className="font-bold text-8xl mt-64 w-full text-center">Skills</h2>
-      <div className="w-full h-screen relative flex items-center justify-center bg-circularLight">
+      <div className="w-full h-screen relative flex items-center justify-center rounded-full bg-circularLight">
         <motion.div
           className="flex items-center justify-center rounded-full font-semibold bg-dark text-light p-8 shadow-dark cursor-pointer"
           whileHover={{ scale: 1.05 }}
         >
           Web
         </motion.div>
+        <Skill name="HTML" x="-25vw" y="2vw" />
+        <Skill name="CSS" x="-5vw" y="-10vw" />
+        <Skill name="Javascript" x="20vw" y="6vw" />
+        <Skill name="ReactJS" x="0vw" y="14vw" />
+        <Skill name="NextJS" x="-20vw" y="-15vw" />
+        <Skill name="Firebase" x="15vw" y="-12vw" />
+        <Skill name="Typescript" x="32vw" y="-5vw" />
+        <Skill name="NodeJS" x="-40vw" y="-1vw" />
+        <Skill name="ExpressJS" x="-2vw" y="-24vw" />
+        <Skill name="MongoDB" x="-25vw" y="18vw" />
+        <Skill name="TailwindCSS" x="18vw" y="18vw" />
       </div>
     </>
   );
