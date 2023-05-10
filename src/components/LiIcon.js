2a0f2e24fe2b024b008627142ef60e31c673fa8a
Chidike Henry <dk1online25@gmail.com>
2023-05-10 19:54:31 +0100

    addres new feat scrollYProgress LiIcon

--- src/components/LiIcon.js
+++ src/components/LiIcon.js
@@ -1,9 +1,38 @@
-import React from 'react'
+import React from "react";
+import { motion, useScroll } from "framer-motion";
 
-const LiIcon = () => {
+const LiIcon = ({ reference }) => {
+  const { scrollYProgress } = useScroll({
+    target: reference,
+    offset: ["center end", "center center"],
+  });
   return (
-    <div>LiIcon</div>
-  )
-}
+    <figure className="absolute left-0 stroke-dark">
+      <svg width="75" height="75" viewBox="0 0 100 100">
+        <circle
+          cx="75"
+          cy="50"
+          r="20"
+          className="stroke-primary stroke-1 fill-none"
+        />
+        <motion.circle
+          cx="75"
+          cy="50"
+          r="20"
+          className="stroke-[5px] fill-light"
+          style={{
+            pathLength : scrollYProgress
+          }}
+        />
+        <circle
+          cx="75"
+          cy="50"
+          r="10"
+          className="stroke-1 fill-none fill-primary"
+        />
+      </svg>
+    </figure>
+  );
+};
 
-export default LiIcon
+export default LiIcon;
