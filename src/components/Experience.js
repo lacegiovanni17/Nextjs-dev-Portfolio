282b70c6760baeee5f0a2b3056a6d126c6bab7a2
Chidike Henry <dk1online25@gmail.com>
2023-05-10 20:06:48 +0100

    added new feat spring

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -7,7 +7,7 @@ const Details = ({ position, company, companyLink, time, address, work }) => {
   return (
       <li ref={ref} className="my-8 first:mt-0 last:mb-0 w-[60%] mx-auto flex flex-col items-center justify-between">
           <LiIcon reference={ref} />
-          <div>
+          <motion.div initial={{y:50}} whileInView={{y:0}} transition={{duration:0.5, type:"spring"}}>
         <h3 className="capitalize font-bold text-2xl">
           {position}&nbsp;
           <a
@@ -22,7 +22,7 @@ const Details = ({ position, company, companyLink, time, address, work }) => {
           {time} | {address}
         </span>
         <p className="font-medium w-full">{work}</p>
-      </div>
+      </motion.div>
     </li>
   );
 };
@@ -41,7 +41,7 @@ const Experience = () => {
         Experience
       </h2>
           <div ref={ref} className="w-[75%] mx-auto relative">
-              <motion.div style={{scaleY: scrollYProgress}} className="absolute left-8 top-0 w-[4px] h-full bg-dark origin-top"/>
+              <motion.div style={{scaleY: scrollYProgress}} className="absolute left-9 top-0 w-[4px] h-full bg-dark origin-top"/>
         <ul className="w-full flex flex-col items-start justify-between ml-4">
           <Details
             position="Software Engineer"
