6daf2a316e977aafb76657287107b0b978cd8763
Chidike Henry <dk1online25@gmail.com>
2023-05-11 11:41:18 +0100

    added new feat projects

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -10,9 +10,20 @@ const projects = () => {
         <title>Chidike | Projects Page</title>
         <meta name="description" content="any description" />
       </Head>
-      <main>
+      <main className="w-full mb-16 flex flex-col items-center justify-center">
         <Layout className="pt-16">
-          <AnimatedText text="Imagination Trumps Knowledge!" />
+                  <AnimatedText text="Imagination Trumps Knowledge!" />
+                  <div className="grid grid-cols-12 gap-24">
+                      <div className="col-span-12">
+                          Featured Project
+                      </div>
+                      <div className="col-span-6">
+                          Project-1
+                      </div>
+                      <div className="col-span-6">
+                          Project-2
+                      </div>
+                  </div>
         </Layout>
       </main>
     </>
