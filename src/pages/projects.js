58d7d90aa9617f066b01824475be034142f02b05
Chidike Henry <dk1online25@gmail.com>
2023-05-11 18:06:20 +0100

    added new feat project1

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,7 +1,44 @@
-import AnimatedText from '@/components/AnimatedText'
-import Layout from '@/components/Layout'
-import Head from 'next/head'
-import React from 'react'
+import AnimatedText from "@/components/AnimatedText";
+import { GithubIcon } from "@/components/Icons";
+import Layout from "@/components/Layout";
+import Head from "next/head";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import project1 from "../../public/images/projects/hengine-blog.jpg";
+
+const FeaturedProject = ({ type, title, summary, img, link, github }) => {
+  return (
+    <article className="w-full flex items-center justify-between rounded-3xl border border-solid border-dark bg-light shadow-2xl p-12">
+      <Link
+        href={link}
+        target="_blank"
+        className="w-1/2 cursor-pointer overflow-hidden rounded-lg"
+      >
+        <Image src={img} alt={title} className="w-full h-auto" />
+      </Link>
+      <div className="w-1/2 flex flex-col items-start justify-between pl-6">
+        <span className="text-primary font-medium text-xl">{type}</span>
+        <Link
+          href={link}
+          target="_blank"
+          className="hover:underline underline-offset-2"
+        >
+          <h2 className="my-2 w-full text-left text-4xl font-bold">{title}</h2>
+        </Link>
+        <p className="my-2 font-medium text-dark">{summary}</p>
+        <div>
+          <Link href={github} target="_blank">
+            <GithubIcon />
+          </Link>
+          <Link href={link} target="_blank">
+            View Project
+          </Link>
+        </div>
+      </div>
+    </article>
+  );
+};
 
 const projects = () => {
   return (
@@ -14,7 +51,21 @@ const projects = () => {
         <Layout className="pt-16">
           <AnimatedText text="Imagination Trumps Knowledge!" />
           <div className="grid grid-cols-12 gap-24">
-            <div className="col-span-12">Featured Project</div>
+            <div className="col-span-12">
+              <FeaturedProject
+                title="NextJS blog web app"
+                img={project1}
+                summary="With featured and recent posts, categories. full markdown articles,
+                author information, comments, and much more, this fully responsive
+                CMS Blog App by Henry is the best Blog Application that
+                you can currently find on the web. And what's best of all is
+                that you and your clients can manage the blog from a dedicated
+                Content Management System with Hygraph"
+                link="https://hengine-blog-1.vercel.app/"
+                github="https://github.com/lacegiovanni17/hengine-blog"
+                type="Featured Project"
+              />
+            </div>
             <div className="col-span-6">Project-1</div>
             <div className="col-span-6">Project-2</div>
             <div className="col-span-12">Featured Project</div>
@@ -25,6 +76,6 @@ const projects = () => {
       </main>
     </>
   );
-}
+};
 
-export default projects
+export default projects;
