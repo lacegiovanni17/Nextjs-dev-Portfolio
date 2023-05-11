389aa4f8c569530635717aa739c0cd3e3f71d772
Chidike Henry <dk1online25@gmail.com>
2023-05-11 11:34:18 +0100

    added new feat projects pgae animated

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,9 +1,22 @@
+import AnimatedText from '@/components/AnimatedText'
+import Layout from '@/components/Layout'
+import Head from 'next/head'
 import React from 'react'
 
 const projects = () => {
   return (
-    <div>projects</div>
-  )
+    <>
+      <Head>
+        <title>Chidike | Projects Page</title>
+        <meta name="description" content="any description" />
+      </Head>
+      <main>
+        <Layout className="pt-16">
+          <AnimatedText text="Imagination Trumps Knowledge!" />
+        </Layout>
+      </main>
+    </>
+  );
 }
 
 export default projects
