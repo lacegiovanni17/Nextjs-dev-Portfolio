1b83435f655458d2d446479f6eb5dc18cc72be0d
Chidike Henry <dk1online25@gmail.com>
2023-05-16 13:49:21 +0100

    added new feat

--- src/pages/articles.js
+++ src/pages/articles.js
@@ -10,10 +10,10 @@ const articles = () => {
           <title>Chidike | Articles Page</title>
           <meta name="description" content="any description" />
             </Head>
-            <main>
+            <main className='w-full mb-16 flex flex-col items-center justify-center overflow-hidden'>
                 <Layout>
                     <AnimatedText text="Words Can Change The World!" className="mb-16" />
-                    <ul>
+                    <ul className='grid grid-col-2 gap-16'>
                         <li>Featured Articles-1</li>
                         <li>Featured Articles-2</li>
                     </ul>
