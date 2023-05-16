3084ae98d49e5814c257f5f2866c5949b7fc3a10
Chidike Henry <dk1online25@gmail.com>
2023-05-16 14:25:53 +0100

    added new feat articles pic

--- src/pages/articles.js
+++ src/pages/articles.js
@@ -1,13 +1,14 @@
-import AnimatedText from '@/components/AnimatedText';
-import Layout from '@/components/Layout';
-import Head from 'next/head';
-import Image from 'next/image';
-import Link from 'next/link';
-import React from 'react'
+import AnimatedText from "@/components/AnimatedText";
+import Layout from "@/components/Layout";
+import Head from "next/head";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import article1 from "../../public/images/articles/create loading screen in react js.jpg";
 
 const FeaturedArticle = ({ img, title, time, summary, link }) => {
   return (
-    <li>
+    <li className="col-span-1 w-full p-4 bg-light border border-solid border-dark rounded-2xl">
       <Link
         href={link}
         target="_blank"
@@ -16,32 +17,39 @@ const FeaturedArticle = ({ img, title, time, summary, link }) => {
         <Image src={img} alt={title} className="w-full h-auto" />
       </Link>
       <Link href={link} target="_blank">
-        <h2 className='capitalize text-2xl font-bold my-2'>{title}</h2>
+        <h2 className="capitalize text-2xl font-bold my-2">{title}</h2>
       </Link>
-      <p className='text-sm mb-2'>{summary}</p>
-      <span className='text-primary'>{ time }</span>
+      <p className="text-sm mb-2">{summary}</p>
+      <span className="text-primary font-semibold">{time}</span>
     </li>
   );
-}
+};
 
 const articles = () => {
-    return (
-      <>
-        <Head>
-          <title>Chidike | Articles Page</title>
-          <meta name="description" content="any description" />
-            </Head>
-            <main className='w-full mb-16 flex flex-col items-center justify-center overflow-hidden'>
-                <Layout>
-                    <AnimatedText text="Words Can Change The World!" className="mb-16" />
-                    <ul className='grid grid-col-2 gap-16'>
-                        <li>Featured Articles-1</li>
-                        <li>Featured Articles-2</li>
-                    </ul>
-                </Layout>
-            </main>
-      </>
-    );
-}
+  return (
+    <>
+      <Head>
+        <title>Chidike | Articles Page</title>
+        <meta name="description" content="any description" />
+      </Head>
+      <main className="w-full mb-16 flex flex-col items-center justify-center overflow-hidden">
+        <Layout>
+          <AnimatedText text="Words Can Change The World!" className="mb-16" />
+          <ul className="grid grid-col-2 gap-16">
+            <FeaturedArticle
+              title="Build A Custom Pagination Component In Reactjs From Scratch"
+              summary="Learn how to build a custom pagination component in ReactJS from scratch. 
+              Follow this step-by-step guide to integrate Pagination component in your ReactJS project."
+              time="9 min read"
+              link="/"
+              img={article1}
+            />
+            <li>Featured Articles-2</li>
+          </ul>
+        </Layout>
+      </main>
+    </>
+  );
+};
 
-export default articles
+export default articles;
