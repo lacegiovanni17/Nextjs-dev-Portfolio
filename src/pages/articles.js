327bd0891dcd80d7cae9f35e3b45ac60b4435507
Chidike Henry <dk1online25@gmail.com>
2023-05-16 14:10:31 +0100

    added new feat

--- src/pages/articles.js
+++ src/pages/articles.js
@@ -1,14 +1,27 @@
 import AnimatedText from '@/components/AnimatedText';
 import Layout from '@/components/Layout';
 import Head from 'next/head';
+import Image from 'next/image';
+import Link from 'next/link';
 import React from 'react'
 
 const FeaturedArticle = ({ img, title, time, summary, link }) => {
   return (
     <li>
-      
+      <Link
+        href={link}
+        target="_blank"
+        className="w-full cursor-pointer overflow-hidden rounded-lg"
+      >
+        <Image src={img} alt={title} className="w-full h-auto" />
+      </Link>
+      <Link href={link} target="_blank">
+        <h2 className='capitalize text-2xl font-bold my-2'>{title}</h2>
+      </Link>
+      <p className='text-sm mb-2'>{summary}</p>
+      <span className='text-primary'>{ time }</span>
     </li>
-  )
+  );
 }
 
 const articles = () => {
