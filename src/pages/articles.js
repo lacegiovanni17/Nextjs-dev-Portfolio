21c552ea6378cdc824b967597cc2ee00b67fed85
Chidike Henry <dk1online25@gmail.com>
2023-05-18 23:19:54 +0100

    added new feat

--- src/pages/articles.js
+++ src/pages/articles.js
@@ -7,20 +7,57 @@ import React from "react";
 import article1 from "../../public/images/articles/pagination component in reactjs.jpg";
 import article2 from "../../public/images/articles/create loading screen in react js.jpg";
 import article3 from "../../public/images/articles/create modal component in react using react portals.png";
-import { motion } from "framer-motion";
+import { motion, useMotionValue } from "framer-motion";
+import { useRef } from "react";
 
 const FramerImage = motion(Image);
 
-const Article = ({img, title, date, link}) => {
+const MovingImg = ({ title, img, link }) => {
+
+  const x = useMotionValue(0);
+  const y = useMotionValue(0);
+  const imgRef = useRef(null);
+
+  function handleMouse(event){
+    imgRef.current.style.display = "inline-block";
+    x.set(event.pageX)
+    y.set(-10)
+  }
+
+  function handleMouseLeave(event){
+    imgRef.current.style.display = "none";
+    x.set(0);
+    y.set(0);
+  }
+
+  return (
+    <Link
+      href={link}
+      target="_blank"
+      onMouseMove={handleMouse}
+      onMouseLeave={handleMouseLeave}
+    >
+      <h2 className="capitalize text-xl font-semibold hover:underline">
+        {title}
+      </h2>
+      <Image
+        ref={imgRef}
+        src={img}
+        alt={title}
+        className="w-96 h-auto hidden absolute rounded-lg"
+      />
+    </Link>
+  );
+};
+
+const Article = ({ img, title, date, link }) => {
   return (
     <li className="relative w-full p-4 py-6 my-4 rounded-xl flex items-center justify-between bg-light text-dark first:mt-0 border border-solid border-dark border-r-4 border-b-4">
-      <Link href={link} target="_blank">
-        <h2>{title}</h2>
-      </Link>
-      <span>{date}</span>
-      </li>
-    )
-}
+      <MovingImg title={title} img={img} link={link} />
+      <span className="text-primary font-semibold pl-4">{date}</span>
+    </li>
+  );
+};
 
 const FeaturedArticle = ({ img, title, time, summary, link }) => {
   return (
@@ -78,29 +115,35 @@ const articles = () => {
               img={article2}
             />
           </ul>
-          <h2 className="font-bold text-4xl w-full text-center my-16 mt-32">All Articles</h2>
+          <h2 className="font-bold text-4xl w-full text-center my-16 mt-32">
+            All Articles
+          </h2>
           <ul>
             <Article
               title="Form Validation In Reactjs: Build A Reusable Custom Hook For Inputs And Error Handling"
               date="May 18, 2023"
               link="/"
-              img={article3} />
+              img={article3}
+            />
           </ul>
-            <Article
-              title="Form Validation In Reactjs: Build A Reusable Custom Hook For Inputs And Error Handling"
-              date="May 18, 2023"
-              link="/"
-              img={article3} />
-            <Article
-              title="Form Validation In Reactjs: Build A Reusable Custom Hook For Inputs And Error Handling"
-              date="May 18, 2023"
-              link="/"
-              img={article3} />
-            <Article
-              title="Form Validation In Reactjs: Build A Reusable Custom Hook For Inputs And Error Handling"
-              date="May 18, 2023"
-              link="/"
-              img={article3} />
+          <Article
+            title="Form Validation In Reactjs: Build A Reusable Custom Hook For Inputs And Error Handling"
+            date="May 18, 2023"
+            link="/"
+            img={article3}
+          />
+          <Article
+            title="Form Validation In Reactjs: Build A Reusable Custom Hook For Inputs And Error Handling"
+            date="May 18, 2023"
+            link="/"
+            img={article3}
+          />
+          <Article
+            title="Form Validation In Reactjs: Build A Reusable Custom Hook For Inputs And Error Handling"
+            date="May 18, 2023"
+            link="/"
+            img={article3}
+          />
         </Layout>
       </main>
     </>
